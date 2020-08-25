c3339aa062fc91d1deffe7891cec97eef4dcdd18
Sera Wang <sakainozomi@icloud.com>
2020-08-26 00:45:55 +0800

    fix: hook test

--- plugin/hook/hook.go
+++ plugin/hook/hook.go
@@ -3,7 +3,10 @@ package example
 import (
 	"context"
 	"github.com/coredns/coredns/plugin/pkg/log"
+	"github.com/coredns/coredns/request"
 	"github.com/miekg/dns"
+	"net"
+	"strconv"
 )
 
 type Hook struct {
@@ -12,6 +15,40 @@ type Hook struct {
 
 func(h Hook)  ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
 	log.Info("request received: %v", r)
+	state := request.Request{W:w, Req:r}
+
+	ans := &dns.Msg{}
+	ans.SetReply(r)
+	ans.Authoritative = true
+
+	ip := state.IP()
+	var rr dns.RR
+
+	switch state.Family() {
+	case 1:
+		rr = new(dns.A)
+		rr.(*dns.A).Hdr = dns.RR_Header{Name: state.QName(), Rrtype: dns.TypeA, Class: state.QClass()}
+		rr.(*dns.A).A = net.ParseIP(ip).To4()
+	case 2:
+		rr = new(dns.AAAA)
+		rr.(*dns.AAAA).Hdr = dns.RR_Header{Name: state.QName(), Rrtype: dns.TypeAAAA, Class: state.QClass()}
+		rr.(*dns.AAAA).AAAA = net.ParseIP(ip)
+	}
+
+	srv := new(dns.SRV)
+	srv.Hdr = dns.RR_Header{Name: "_"+state.Proto()+"."+state.QName(), Rrtype: dns.TypeSRV, Class: state.QClass()}
+	if state.QName() == "." {
+		srv.Hdr.Name = "_"+state.Proto()+state.QName()
+	}
+	port, _ := strconv.Atoi(state.Port())
+	srv.Port = uint16(port)
+	srv.Target = "."
+	ans.Extra = []dns.RR{rr, srv}
+
+	err := w.WriteMsg(ans)
+	if err != nil {
+		log.Error("[HOOK] write msg error: "+err.Error())
+	}
 
 	return 0, nil
 }
